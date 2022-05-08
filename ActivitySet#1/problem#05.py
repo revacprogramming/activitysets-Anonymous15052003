e3913c94c81f5409014427dcaff6be2163160e3f
Anonymous15052003 <sharonpro24x7@gmail.com>
2022-05-08 21:16:47 +0530

    Score if else problem

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,26 @@
 # Functions
 
+# 3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
+# Score Grade
+# >= 0.9 A
+# >= 0.8 B
+# >= 0.7 C
+# >= 0.6 D
+# < 0.6 F
+# If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
 
-def computepay(h, r):
-    pass  # ...
 
+a=float(input("Enter Score : "))
 
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+if  a >= 0.9 and a <= 1.0:
+    print("A grade")
+elif a >= 0.8 and a < 0.9:
+    print("B grade")
+elif a >= 0.7 and a < 0.8:
+    print("C grade")
+elif a >= 0.6 and a < 0.7:
+    print("D grade")
+elif a < 0.6 and a >= 0.0:
+    print("F grade")
+else:
+    print("Wrong Score Entered !")
