bd96367eaf9d90fbaaf24a280239e3d69f2b7bb7
Anonymous15052003 <sharonpro24x7@gmail.com>
2022-05-23 09:00:30 +0530

    set 2 problem 1 completed

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,16 @@
-
-
 def add(a, b):
-    return  # ...
+    c=a+b
+    return c
 
 
 def main():
-    a = input  # ...
-    b = input  # ...
+    a = int(input("Enter 1st number : "))
+    b = int(input("Enter 2nd number : "))
 
     c = add(a, b)
-    print  # ...
+    print(c)
+
+main()
+
+''' if __name__=="__main__":
+    main() '''
