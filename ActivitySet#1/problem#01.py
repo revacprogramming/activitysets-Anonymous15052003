b44efde7803617506e55a4eaddd88e8f95fa8fe3
Anonymous15052003 <sharonpro24x7@gmail.com>
2022-05-25 15:33:37 +0530

    count program

---
+++ ActivitySet#1/problem#01.py
@@ -0,0 +1,18 @@
+name = input("Enter the file name : ")
+
+file = open(name)
+
+count = 0
+sum = 0
+
+for i in file:
+    if not i.startswith("X-DSPAM-Confidence:"):
+        continue
+    t = i.find("0")
+    num = i[t:]
+    number = float(num)
+    sum=sum + number
+    count = count + 1
+
+avg = sum/count
+print("Average spam confidence:",avg)
