f38d8e562520d228be5accf1572803078c20a0a0
Anonymous15052003 <sharonpro24x7@gmail.com>
2022-05-28 22:36:14 +0530

    rectangle

---
+++ ActivitySet#3/problem#1.py
@@ -0,0 +1,7 @@
+# Program to find the area of a rectangle formed by three coordinates
+
+n=int(input("Enter how many reactangle coordinates you want to input : "))
+
+for i in range(n+1):
+    for j in range(n+1):
+        x=float(input("Enter "))
