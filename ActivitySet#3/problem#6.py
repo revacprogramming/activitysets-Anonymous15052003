87c0d91db1b3084b7dad89f44fbded4de79f2fbe
Anonymous15052003 <sharonpro24x7@gmail.com>
2022-06-01 15:36:33 +0530

    day 9

---
+++ ActivitySet#3/problem#6.py
@@ -0,0 +1,18 @@
+# (1).__add__(1)
+
+# print(abs(-1.32))
+
+# def fop(*arg,**kwargs):
+#     print(ags,*args,kwargs,**kwargs)
+
+
+# fop(1,2,3,a=2,c=4)
+
+
+def hi(*hi):
+
+    for i in hi:
+        print(i)
+
+        
+hi(2,3,4)
