1e6bb0ccf29a4d1f04c339e0810d7cd9db7d51ef
Anonymous15052003 <sharonpro24x7@gmail.com>
2022-06-06 10:12:44 +0530

    exchanging values

---
+++ ActivitySet#3/ActivitySet#4/problem#11.py
@@ -0,0 +1,9 @@
+x=int(input("Enter first number :"))
+y=int(input("Enter second number :"))
+
+print("After exchanging numbers :- ")
+
+x,y=y,x
+
+print("The first number is ",x)
+print("The second number is ",y)
